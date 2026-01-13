5ffbcb3fa886f5d7297cfefd59955e5b23aeda46
KozmunkasKalman <kalmankozmunkas@gmail.com>
2026-01-13 17:48:49 +0100

    small fixes here and there, i will not commit again until i figure out how to implement utf8 support

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ install_bin() {
   echo -ne "[ ] Moving the Kaczynski binary to /usr/bin/     (2/3)\n"
   sudo cp bin/kcz /usr/bin/kcz
   if [ $? -eq 0 ]; then
-    tput cuu 2
+    tput cuu 1
     echo -ne "\r[\033[0;32m+\033[0m] Kaczynski-TED installation successful.       (2/3)\n"
     cfgfile
   else
-    tput cuu 2
+    tput cuu 1
     echo -ne "\r[\033[0;31m-\033[0m] Kaczynski-TED installation failed.                \n"
     echo -ne "    Please make sure /usr/bin/kcz is not in use and that you have the neccessary permissions.\n"
   fi
